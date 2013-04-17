d1de0c5bd5c4b2a3823daa7fcf27ddae3893992c
Bob Fanger <bfanger@gmail.com>
2013-04-17 10:18:21 +0200

    Initial commit
    yo angular
    cakephp-style folders.
    pepertale styling

---
+++ Gruntfile.js
@@ -0,0 +1,261 @@
+'use strict';
+var lrSnippet = require('grunt-contrib-livereload/lib/utils').livereloadSnippet;
+var mountFolder = function (connect, dir) {
+  return connect.static(require('path').resolve(dir));
+};
+
+module.exports = function (grunt) {
+  // load all grunt tasks
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+  grunt.initConfig({
+    watch: {
+      compass: {
+        files: ['webroot/sass/{,*/}*.{scss,sass}'],
+        tasks: ['compass']
+      },
+      livereload: {
+        files: [
+          'webroot/{,*/}*.html',
+          '{.tmp,webroot}/css/{,*/}*.css',
+          '{.tmp,webroot}/js/{,*/}*.js',
+          'webroot/img/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
+        ],
+        tasks: ['livereload']
+      }
+    },
+    connect: {
+      options: {
+        port: 9000,
+        // Change this to '0.0.0.0' to access the server from outside.
+        hostname: 'localhost'
+      },
+      livereload: {
+        options: {
+          middleware: function (connect) {
+            return [
+              lrSnippet,
+              mountFolder(connect, '.tmp'),
+              mountFolder(connect, 'webroot')
+            ];
+          }
+        }
+      },
+      test: {
+        options: {
+          middleware: function (connect) {
+            return [
+              mountFolder(connect, '.tmp'),
+              mountFolder(connect, 'test')
+            ];
+          }
+        }
+      }
+    },
+    open: {
+      server: {
+        url: 'http://localhost:<%= connect.options.port %>'
+      }
+    },
+    clean: {
+      dist: {
+        files: [{
+          dot: true,
+          src: [
+            '.tmp',
+            'dist/*',
+            '!dist/.git*'
+          ]
+        }]
+      },
+      server: '.tmp'
+    },
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc'
+      },
+      all: [
+        'Gruntfile.js',
+        'webroot/js/{,*/}*.js'
+      ]
+    },
+    karma: {
+      unit: {
+        configFile: 'karma.conf.js',
+        singleRun: true
+      }
+    },
+    compass: {
+      options: {
+        basePath: 'webroot',
+        sassDir: 'sass',
+        cssDir: '../.tmp/css',
+        imagesDir: 'img',
+        javascriptsDir: 'js',
+        fontsDir: 'fonts',
+        importPath: 'webroot/components',
+        relativeAssets: true
+      },
+      dist: {},
+      server: {
+        options: {
+          debugInfo: true
+        }
+      }
+    },
+    concat: {
+      dist: {
+        files: {
+          'dist/js/scripts.js': [
+            '.tmp/js/{,*/}*.js',
+            'webroot/js/{,*/}*.js'
+          ]
+        }
+      }
+    },
+    useminPrepare: {
+      html: 'webroot/index.html',
+      options: {
+        dest: 'dist'
+      }
+    },
+    usemin: {
+      html: ['dist/{,*/}*.html'],
+      css: ['dist/css/{,*/}*.css'],
+      options: {
+        dirs: ['dist']
+      }
+    },
+    imagemin: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: 'webroot/images',
+          src: '{,*/}*.{png,jpg,jpeg}',
+          dest: 'dist/images'
+        }]
+      }
+    },
+    cssmin: {
+      dist: {
+        files: {
+          'dist/css/main.css': [
+            '.tmp/css/{,*/}*.css',
+            'webroot/css/{,*/}*.css'
+          ]
+        }
+      }
+    },
+    htmlmin: {
+      dist: {
+        options: {
+          /*removeCommentsFromCDATA: true,
+          // https://github.com/yeoman/grunt-usemin/issues/44
+          //collapseWhitespace: true,
+          collapseBooleanAttributes: true,
+          removeAttributeQuotes: true,
+          removeRedundantAttributes: true,
+          useShortDoctype: true,
+          removeEmptyAttributes: true,
+          removeOptionalTags: true*/
+        },
+        files: [{
+          expand: true,
+          cwd: 'webroot',
+          src: ['*.html', 'views/*.html'],
+          dest: 'dist'
+        }]
+      }
+    },
+    cdnify: {
+      dist: {
+        html: ['dist/*.html']
+      }
+    },
+    ngmin: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: 'dist/scripts',
+          src: '*.js',
+          dest: 'dist/scripts'
+        }]
+      }
+    },
+    uglify: {
+      dist: {
+        files: {
+          'dist/js/scripts.js': [
+            'dist/js/scripts.js'
+          ],
+        }
+      }
+    },
+    rev: {
+      dist: {
+        files: {
+          src: [
+            'dist/js/{,*/}*.js',
+            'dist/css/{,*/}*.css',
+            'dist/img/{,*/}*.{png,jpg,jpeg,gif,webp}',
+            'dist/css/fonts/*'
+          ]
+        }
+      }
+    },
+    copy: {
+      dist: {
+        files: [{
+          expand: true,
+          dot: true,
+          cwd: 'webroot',
+          dest: 'dist',
+          src: [
+            '*.{ico,txt}',
+            '.htaccess',
+            'components/**/*',
+            'img/{,*/}*.{gif,webp}'
+          ]
+        }]
+      }
+    }
+  });
+
+  grunt.renameTask('regarde', 'watch');
+
+  grunt.registerTask('server', [
+    'clean:server',
+    'compass:server',
+    'livereload-start',
+    'connect:livereload',
+    'open',
+    'watch'
+  ]);
+
+  grunt.registerTask('test', [
+    'clean:server',
+    'compass',
+    'connect:test',
+    'karma'
+  ]);
+
+  grunt.registerTask('build', [
+    'clean:dist',
+    'jshint',
+    'test',
+    'compass:dist',
+    'useminPrepare',
+    'imagemin',
+    'cssmin',
+    'htmlmin',
+    'concat',
+    'copy',
+    'cdnify',
+    'ngmin',
+    'uglify',
+    'rev',
+    'usemin'
+  ]);
+
+  grunt.registerTask('default', ['build']);
+};
