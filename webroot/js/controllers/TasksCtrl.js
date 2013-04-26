f909f5b9fa9b3b6a1913f03efb54eb733fccb8bb
Bob Fanger <bob.fanger@noprotocol.nl>
2013-04-26 11:36:29 +0200

    Excel-style input

--- webroot/js/controllers/TasksCtrl.js
+++ webroot/js/controllers/TasksCtrl.js
@@ -2,32 +2,35 @@
 app.controller('TasksCtrl', function ($scope, Task) {
 	'use strict';
 
-    $scope.tasks = Task.query();
+	$scope.tasks = Task.query();
+
+	// Add tasks automaticly (always show an empty row at the bottom, excel-style)
+	$scope.$watch('tasks', function (tasks) {
+		if (tasks.lenght === 0 || tasks[tasks.length - 1].title !== '') {
+			tasks.push(new Task({
+				title: '',
+				estimate: ''
+			}));
+			// @todo recalc burndown?
+			$scope.chartData.push(['2008', Math.random() * 1500, Math.random() * 1500]);
+		}
+	}, true);
 
 	$scope.sum = function() {
 		return Task.totalEstimated($scope.tasks);
-	}
+	};
 
 	$scope.sortStopped = function() {
 		console.log($scope);
 		// @todo update sortorder fields.
 	};
 
-	// @todo Add tasks automaticly (always show an empty row at the bottom, excel-style)
-	$scope.addTask = function () {
-		$scope.tasks.push(new Task({
-			title: 'Untitled',
-			estimate: 0
-		}));
-		$scope.chartData.push(['2008', Math.random() * 1500, Math.random() * 1500]);
-	};
-
 	// @todo calculate chartdata based on timeline & estimates.
 	$scope.chartData = [
 		['Year', 'Dummy', 'Waarden'],
-		['2004',  1000,      400],
-   		['2005',  1170,      460],
-   		['2006',  660,       1120],
-   		['2007',  Math.random() * 1500,      Math.random() * 1500]
-   	];
+		['2004',  1000,	  400],
+		['2005',  1170,	  460],
+		['2006',  660,	   1120],
+		['2007',  Math.random() * 1500,	  Math.random() * 1500]
+	];
 });
